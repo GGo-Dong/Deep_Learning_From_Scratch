4f0f997e44264a863feea234a0666361df32b001
KimDongSu <kimdongsu481@gmail.com>
2026-01-19 17:47:34 +0900

    softmax 안정화

--- 1-3/active_function.py
+++ 1-3/active_function.py
@@ -13,8 +13,9 @@ def step_function(x):
     return np.array(x > 0, dtype=np.int32)
 
 def softmax(x):
-    exp_x = np.exp(x)
+    c = np.max(x)
+    exp_x = np.exp(x-c) # 오버플로우를 막기 위해 c를 빼줌
     sum_exp_x = np.sum(exp_x)
-    y = exp_x/sum_exp_x
-
+    y = exp_x / sum_exp_x
+    
     return y
