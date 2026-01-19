ee24e5fc08cfad4c2b3ce701998659ebf86ef375
KimDongSu <kimdongsu481@gmail.com>
2026-01-19 18:08:25 +0900

    dataset & mnist_show.py

---
+++ 1-3/mnist_show.py
@@ -0,0 +1,21 @@
+import sys, os
+sys.path.append(os.pardir)
+import numpy as np
+from dataset.mnist import load_mnist
+from PIL import Image
+
+def img_show(img):
+    pil_img = Image.fromarray(np.uint8(img))
+    pil_img.show()
+
+(x_train, t_train), (x_test, t_test) = load_mnist(flatten=True, normalize=False)
+
+img = x_train[0] # 테스트로 출력해 볼 이미지
+label = t_train[0] # 테스트로 출력해 볼 레이블
+print(label) # 5
+
+print(img.shape)
+img = img.reshape(28, 28) # flatten으로 인해 1차원으로 변한 이미지를 원래 모양인 28x28 로 되돌림
+print(img.shape)
+
+img_show(img)
