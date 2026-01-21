e48405f55ffaf7ea967dec88463482f3cf9c7582
KimDongSu <kimdongsu481@gmail.com>
2026-01-21 11:16:05 +0900

    cross_entropy_error

--- common/functions.py
+++ common/functions.py
@@ -19,3 +19,11 @@ def softmax(x):
     y = exp_x / sum_exp_x
     
     return y
+
+def cross_entropy_error(y, t):
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+    
+    batch_size = y.shape[0]
+    return -np.sum(t*np.log(y)) / batch_size
