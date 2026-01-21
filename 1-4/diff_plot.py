ba9f890043cbfcb84086d4672dcc6204c2ecf267
KimDongSu <kimdongsu481@gmail.com>
2026-01-21 11:27:29 +0900

    diff_plot

---
+++ 1-4/diff_plot.py
@@ -0,0 +1,13 @@
+from numerical_diff import numerical_diff
+import numpy as np
+import matplotlib.pyplot as plt
+
+def function_1(x):
+    return 0.01*x**2 + 0.1*x
+
+x = np.arange(0.0, 20.0, 0.1)
+y = function_1(x)
+plt.xlabel("x")
+plt.ylabel("y")
+plt.plot(x,y)
+plt.show()
